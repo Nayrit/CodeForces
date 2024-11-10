f6354b5fbdd9e871989f474ca19fc6273d942bec
Nayrit <110413462+Nayrit@users.noreply.github.com>
2024-11-10 07:27:54 +0600

    Add files via upload

---
+++ A_Codeforces_Checking.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        char s;
+        cin>>s;
+        if(s=='c' || s=='o' || s=='d' || s=='e' || s=='f' || s=='r' || s=='s') cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
